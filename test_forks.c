af4dc9a2e03c2b081e910f46c1f4f0aa6849d5e0
Alexander Holbreich <alexander@holbreich.org>
2024-05-05 11:57:24 +0200

    refactor: small improvement to make the example be more understandable

--- test_forks.c
+++ test_forks.c
@@ -3,37 +3,44 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
-const int SLEEP_CHILD = 3;
+const int SLEEP_CHILD = 2;
 const int SLEEP_PARENT = 1;
 const int ROUNDS = 10;
 
+/**
+ * Demonstration of fork behavior (Linux)
+*/
 int main ()
 {
-   printf ("Starting Process: " );
+   printf ("Starting Process: \n" );
    int i, j;
    pid_t pid;
 
-   pid = fork();
+   pid = fork(); // Now there are two processes running one is parent and another child
 
-   if (pid == 0)
+   if (pid == 0) // if fork returns 0, so we are in the child process.    
    {
-      //if fork returnes 0, we are in the child process.
-       
+      // Code that will be applied in child only
       for (j=0; j < ROUNDS; j++)
       {
-        printf ("Childprocess:  %d (PID: %d)\n", j, getpid());
+        printf ("Child process:  %d (PID: %d)\n", j, getpid());
         sleep (SLEEP_CHILD);
       }
+      printf ("Child process done. \n" );
       exit (0);
    }
    else if (pid > 0)
    {
-      //if fork returnes 1, we are in the Parent process.
+      //if fork returns 1, we are in the parent process.
       for (i=0; i < ROUNDS; i++)
       {
-         printf ("Parentprocess: %d (PID: %d)\n", i, getpid());
+         printf ("Parent process: %d (PID: %d)\n", i, getpid());
          sleep (SLEEP_PARENT);
       }
+      
+      printf ("Parent process done\n" );
+      exit (0);
+
    }
    else
    {
